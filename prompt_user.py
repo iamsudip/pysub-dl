7b247754ca308a2ab26af60668ca32c4c8e565ca
Iam sudip <iamsudip@programmer.net>
2013-08-24 02:01:21 +0530

    Added prompt_again() function

--- prompt_user.py
+++ prompt_user.py
@@ -4,3 +4,8 @@ def prompt():
     ''' Returns response from user.
     '''
     return raw_input('Was that appropriate? ')
+
+def prompt_again():
+    ''' Returns response from user.
+    '''
+    return raw_input('Enter the number respective to download subtitle: ')
