ef20c35bc6f310a24081b40419362cd5e0033808
Iam sudip <iamsudip@programmer.net>
2013-08-26 23:15:27 +0530

    Added new function prompt2()

--- prompt_user.py
+++ prompt_user.py
@@ -5,7 +5,12 @@ def prompt():
     '''
     return raw_input('Was that appropriate? ')
 
-def prompt_again():
+def prompt1():
     ''' Returns response from user.
     '''
     return raw_input('Enter the number respective to download subtitle: ')
+
+def prompt2():
+    ''' Returns response from user.
+    '''
+    return raw_input('Choose the movie from the list. Enter the corresponding number: ')
