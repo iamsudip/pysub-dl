3b239b8cf55be346c385eadbeb65d4e37e9ef79e
Iam sudip <iamsudip@programmer.net>
2013-09-09 19:49:12 +0530

    Updated version info

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
     requirements.append('requests')
 
 setup(name = 'pysub-dl',
-    version = '0.1.9',
+    version = '0.2.0',
     description = "Script to download movie subtitles automatically",
     long_description = long_description,
     platforms = ["Linux"],
