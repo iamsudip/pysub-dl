ebc24a7e528200176235754dc9592c967f54c78b
iamsudip <iamsudip@programmer.net>
2013-10-24 22:58:11 +0530

    Added support for python >= 2.3

--- setup.py
+++ setup.py
@@ -18,9 +18,13 @@ try:
     import requests
 except ImportError:
     requirements.append('requests')
+try:
+    import argparse
+except ImportError:
+    requirements.append('argparse')
 
 setup(name = 'pysub-dl',
-    version = '0.2.8',
+    version = '0.2.9',
     description = "Script to download movie subtitles automatically",
     long_description = long_description,
     platforms = ["Linux"],
@@ -31,8 +35,9 @@ setup(name = 'pysub-dl',
     packages = find_packages(),
     install_requires = requirements,
     dependency_links = ['https://pypi.python.org/pypi/requests/1.2.3',
-                        'https://pypi.python.org/pypi/BeautifulSoup/3.2.1',
-                        'https://pypi.python.org/pypi/setuptools/0.6c11'
+        'https://pypi.python.org/pypi/BeautifulSoup/3.2.1',
+        'https://pypi.python.org/pypi/setuptools/0.6c11',
+        'https://pypi.python.org/pypi/argparse/1.2.1'
 ],
     include_package_data = True,
     scripts = ['pysub-dl', 'download_it.py', 'prompt_user.py'],
